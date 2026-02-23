ae28e86adf714d5c96651a8a3e7b25366bcdb2f3
Vyacheslav Sudakov <vyacheslav.sudakov@ntprog.com>
2026-02-23 21:49:43 +0300

    add CustomUser and UserPermission models

--- common_core/enums/int_enum_with_labels.py
+++
@@ -1,9 +0,0 @@
-from enum import IntEnum
-
-
-class IntEnumWithLabels(IntEnum):
-    def __new__(cls, value: int, label: str):
-        obj = int.__new__(cls, value)
-        obj._value_ = value
-        obj.label = label
-        return obj
---
+++ users/models/custom_user/models.py
@@ -0,0 +1,42 @@
+from typing import ClassVar
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+from users.enums import UserRole, Gender
+from users.custom_manager import CustomUserManager
+from users.models.permissions import CustomPermissionsMixin
+from uuid import uuid4 as uuid
+
+
+class CustomUser(AbstractBaseUser, CustomPermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid, editable=False)
+    email = models.EmailField(unique=True)
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    patronymic = models.CharField(max_length=30, blank=True)
+
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.as_django_model_choices(),
+        default=Gender.Male
+    )
+
+    role = models.CharField(
+        max_length=30,
+        choices=UserRole.as_django_model_choices(),
+        default=UserRole.Client
+    )
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS: ClassVar[list[str]] = []
+
+    objects = CustomUserManager()
