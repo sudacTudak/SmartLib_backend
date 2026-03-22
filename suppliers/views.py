cdf5b89d6314fd46c7a408a0c341891bf6dd4136
Vyacheslav Sudakov <vyacheslav.sudakov@ntprog.com>
2026-03-22 09:57:32 +0300

    add query param for suppliers

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel, Field, ValidationError
+from rest_framework.exceptions import ParseError
 from rest_framework.mixins import (
     RetrieveModelMixin,
     ListModelMixin,
@@ -5,7 +7,7 @@ from rest_framework.mixins import (
     UpdateModelMixin,
     DestroyModelMixin,
 )
-from rest_framework.permissions import AllowAny, SAFE_METHODS
+from rest_framework.permissions import SAFE_METHODS
 
 from common_core.classes import ViewSetBase
 from suppliers.models import Supplier
@@ -13,7 +15,15 @@ from suppliers.serializers import SupplierSerializer
 from users.enums import UserPermissions
 from users.permissions import IsStaff, HasUserPermission
 
-__all__ = ['SupplierViewSet']
+__all__ = ['SupplierViewSet', 'SupplierListQueryParams']
+
+
+class SupplierListQueryParams(BaseModel):
+    name: str | None = Field(None)
+
+    model_config = {
+        'populate_by_name': True,
+    }
 
 
 class SupplierViewSet(
@@ -26,6 +36,19 @@ class SupplierViewSet(
 ):
     serializer_class = SupplierSerializer
     queryset = Supplier.objects.all()
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        try:
+            params = SupplierListQueryParams.model_validate(self.get_raw_query_params())
+        except ValidationError as exc:
+            raise ParseError(detail=exc.errors())
+
+        if name := params.name:
+            qs = qs.filter(name__icontains=name)
+
+        return qs
 
     def get_permissions(self):
         if self.request.method in SAFE_METHODS:
